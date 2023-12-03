ff39a40aa0d17743a693aed30bc584160951b39d
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 22:39:06 +0100

    fix again the classes from bindning feture that does not com in main branch

---
+++ folderDrive/src/main/java/com/example/folderDrive/Controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.folderDrive.controller;
+
+import com.example.folderDrive.dto.AuthenticationRequest;
+import com.example.folderDrive.dto.AuthenticationResponse;
+import com.example.folderDrive.dto.RegisterRequest;
+import com.example.folderDrive.service.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @Valid @RequestBody RegisterRequest request
+    ){
+        return ResponseEntity.ok(authenticationService.register(request));
+
+    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> login(
+            @Valid @RequestBody AuthenticationRequest request
+    ){
+        return ResponseEntity.ok(authenticationService.login(request));
+
+    }
+
+
+
+}
