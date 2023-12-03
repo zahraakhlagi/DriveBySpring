5bb10c762f96db3ad2952036353187841414478d
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 21:20:15 +0100

    add two endpoints to register and login

---
+++ folderDrive/src/main/java/com/example/folderDrive/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.folderDrive.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+         @Valid @RequestBody RegisterRequest request
+    ){
+
+    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @Valid @RequestBody AuthenticateRequest request
+    ){
+
+    }
+
+
+
+}
