9340e90953a94edb30632be41e7e07807955592b
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 11:52:53 +0100

    commit

---
+++ folderDrive/src/main/java/com/example/folderDrive/DrivefolderApplication.java
@@ -0,0 +1,13 @@
+package com.example.folderDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivefolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivefolderApplication.class, args);
+	}
+
+}
---
+++ folderDrive/src/test/java/com/example/folderDrive/DrivefolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.folderDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivefolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
