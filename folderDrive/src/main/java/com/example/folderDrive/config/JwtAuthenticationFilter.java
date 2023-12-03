adb99b4076cd74d17ba679a50099b60d3eb39733
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 13:41:42 +0100

    jwt starts with 7

--- folderDrive/src/main/java/com/example/folderDrive/config/JwtAuthenticationFilter.java
+++ folderDrive/src/main/java/com/example/folderDrive/config/JwtAuthenticationFilter.java
@@ -1,2 +1,28 @@
-package com.example.folderDrive.config;public class JwtAuthenticationFilter {
+package com.example.folderDrive.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+           @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+       final String authHeader= request.getHeader("Authorization");
+       final String jwt;
+        if (authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt= authHeader.substring(7);
+
+    }
 }
