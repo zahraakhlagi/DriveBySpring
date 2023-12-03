f64911803d379a2c27f991a6a6a18b4fff01b818
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 15:47:01 +0100

    if statement to check if we have username and it doesnt authenticated yet we should load the username in database

--- folderDrive/src/main/java/com/example/folderDrive/config/JwtAuthenticationFilter.java
+++ folderDrive/src/main/java/com/example/folderDrive/config/JwtAuthenticationFilter.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -14,6 +17,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -30,6 +34,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         jwt= authHeader.substring(7);
         //userEmail=//todo extract the user email from jwt token
         userEmail= jwtService.extractUsername(jwt);
+        //authentication==null means that user is not connected yet
+        if (userEmail!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails= this.userDetailsService.loadUserByUsername(userEmail);
+
+        }
 
     }
 }
