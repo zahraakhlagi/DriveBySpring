e9afe0a8a153be16e50a4f381a624773733e1db5
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 14:15:27 +0100

    generate a sectet key and decode this key by BASE64 algotithm

--- folderDrive/src/main/java/com/example/folderDrive/service/JwtService.java
+++ folderDrive/src/main/java/com/example/folderDrive/service/JwtService.java
@@ -2,8 +2,13 @@ package com.example.folderDrive.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
 
 public class JwtService {
+    private static final String SECRET_KEY="fbWRpwB1JzTboXyqi7Ugg6PF/xjGjJ7ldLboV0eDNrxzihV8GTi3SysyQpe+I+Vy";
     public String extractUsername(String token) {
         return  null;
     }
@@ -15,5 +20,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJwt(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
