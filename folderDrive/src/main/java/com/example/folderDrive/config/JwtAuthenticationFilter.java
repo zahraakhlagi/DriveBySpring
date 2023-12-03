bb6f1c1cd9c5f1f2ba90a694d3816feb1587eada
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 14:07:18 +0100

    creat a extractAllClaims to get body from token

--- folderDrive/src/main/java/com/example/folderDrive/config/JwtAuthenticationFilter.java
+++ folderDrive/src/main/java/com/example/folderDrive/config/JwtAuthenticationFilter.java
@@ -1,14 +1,17 @@
 package com.example.folderDrive.config;
 
+import com.example.folderDrive.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     @Override
--- folderDrive/src/main/java/com/example/folderDrive/service/JwtService.java
+++ folderDrive/src/main/java/com/example/folderDrive/service/JwtService.java
@@ -1,4 +1,19 @@
 package com.example.folderDrive.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 public class JwtService {
+    public String extractUsername(String token) {
+        return  null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
 }
