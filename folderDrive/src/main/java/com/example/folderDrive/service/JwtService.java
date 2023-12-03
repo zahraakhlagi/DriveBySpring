3c7df063815b8af6dd9c1ae770d668c3bd674eb9
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 15:01:27 +0100

    create a isTokenInvalid method, to check the username from the userdetails is the same as username from token and check the token is not expired

--- folderDrive/src/main/java/com/example/folderDrive/service/JwtService.java
+++ folderDrive/src/main/java/com/example/folderDrive/service/JwtService.java
@@ -40,6 +40,11 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username= extractUsername(token);
+        return(username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
