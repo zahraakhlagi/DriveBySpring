cb3bf5b20b652649ab37c6ee82998c294e44b099
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 16:41:29 +0100

    create a userService

---
+++ folderDrive/src/main/java/com/example/folderDrive/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.folderDrive.service;
+
+import com.example.folderDrive.repositoreis.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("User not found")) ;
+    }
+}
