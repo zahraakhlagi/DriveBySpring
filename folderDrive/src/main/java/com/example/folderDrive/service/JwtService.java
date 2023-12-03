ee34461c3a26bd7e98b27fff6cde68908fb965b2
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 14:53:47 +0100

    create generateToken method

--- folderDrive/src/main/java/com/example/folderDrive/service/JwtService.java
+++ folderDrive/src/main/java/com/example/folderDrive/service/JwtService.java
@@ -2,10 +2,16 @@ package com.example.folderDrive.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 public class JwtService {
@@ -16,6 +22,22 @@ public class JwtService {
     public <T> T extractClaims(String token, Function<Claims,T> claimsResolver){
         final Claims claims= extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+    public String generateToken(
+            Map<String, Objects> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000*60*24))
+                .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token){
