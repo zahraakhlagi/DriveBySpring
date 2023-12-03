f415b3d4c21dec82c1625e3828e26070c05bd0f1
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 21:42:29 +0100

    create a register method in authenticationService, in this method : create a new user, save in db and generate a token for this user

--- folderDrive/src/main/java/com/example/folderDrive/controller/AuthController.java
+++ folderDrive/src/main/java/com/example/folderDrive/controller/AuthController.java
@@ -3,7 +3,9 @@ package com.example.folderDrive.controller;
 import com.example.folderDrive.dto.AuthenticateRequest;
 import com.example.folderDrive.dto.AuthenticationResponse;
 import com.example.folderDrive.dto.RegisterRequest;
+import com.example.folderDrive.service.AuthenticationService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,20 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
          @Valid @RequestBody RegisterRequest request
     ){
+        return ResponseEntity.ok(authenticationService.register(request));
 
     }
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<AuthenticationResponse> login(
             @Valid @RequestBody AuthenticateRequest request
     ){
+        return ResponseEntity.ok(authenticationService.login(request));
 
     }
 
---
+++ folderDrive/src/main/java/com/example/folderDrive/service/AuthenticationService.java
@@ -0,0 +1,2 @@
+package com.example.folderDrive.service;public class AuthenticationService {
+}
