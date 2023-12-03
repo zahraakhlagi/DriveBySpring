2c2bac01ede71e1d78aebb5c929685d59921b72f
zahraakhlagi <zahra.akhlagi2016@gmail.com>
2023-12-03 13:15:26 +0100

    check if we have a token

---
+++ folderDrive/src/main/java/com/example/folderDrive/config/JwtAuthentificationFilter.java
@@ -0,0 +1,31 @@
+package com.example.folderDrive.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class JwtAuthentificationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+       @NonNull HttpServletRequest request,
+       @NonNull HttpServletResponse response,
+       @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader= request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+    }
+}
